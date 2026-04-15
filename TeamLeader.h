4a9635d783148189da900072c3d03b7f0b02c05c
osherwin-1 <olive@DESKTOP-7OL15UE>
2026-04-15 19:13:00 -0400

    Created TeamLeader header file (copilot assisted)

--- TeamLeader.h
+++ TeamLeader.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef TEAMLEADER_H
+#define TEAMLEADER_H
+
+#include "ProductionWorker.h"
+
+class TeamLeader : public ProductionWorker {
+private:
+	double monthlyBonus;
+	int requiredTrainingHours;
+	int attendedTrainingHours;
+
+public:
+	TeamLeader();
+	TeamLeader(string n, string num, string date, int s, double rate, double bonus, int reqHours, int attHours);
+	void setMonthlyBonus(double bonus);
+	void setRequiredTrainingHours(int hours);
+	void setAttendedTrainingHours(int hours);
+	double getMonthlyBonus() const;
+	int getRequiredTrainingHours() const;
+	int getAttendedTrainingHours() const;
+	void printTeamLeader() const;
+};
+#endif
