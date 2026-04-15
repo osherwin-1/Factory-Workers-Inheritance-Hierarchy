fc8889d14393afa6201c9191a7b249848914f5d9
osherwin-1 <olive@DESKTOP-7OL15UE>
2026-04-15 18:58:50 -0400

    Created production worker header file

--- ProductionWorker.h
+++ ProductionWorker.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef PRODUCTIONWORKER_H
+#define PRODUCTIONWORKER_H
+
+#include "Employee.h"
+
+class ProductionWorker : public Employee {
+private:
+	int shift;
+	double hourlyPayRate;
+
+public:
+	ProductionWorker();
+	ProductionWorker(string n, string num, string date, int s, double rate);
+	
+	void setShift(int s);
+	void setHourlyPay(double rate);
+
+	int getShift() const;
+
+	double getHourlyPay() const;
+
+	void printProductionWorker() const;
+};
+#endif
